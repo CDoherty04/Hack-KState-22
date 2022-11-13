8c642b18e42e0e961fe62db322e9039def004111
CDoherty04 <93807752+CDoherty04@users.noreply.github.com>
2022-11-13 06:29:17 -0600

    Migrate Correct Files
    Published finalized, correct files.

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-from flask import Flask
-from views import views
+"""
 
-app = Flask(__name__)
-app.register_blueprint(views, url_prefix="/views")
+Python prototype terminal version of program
 
 
-if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+"""
+
 
 # Beginner: I’m just starting to learn tennis
 # Intermediate: I used to play tennis or started playing recently and want to keep improving
